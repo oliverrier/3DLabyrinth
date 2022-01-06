5958bb7719298af292764398669379410fed1876
oliverrier <olivier.verrier@ynov.com>
2022-01-06 16:38:05 +0100

    Add board rotation

---
+++ Assets/Scripts/InitGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitGame : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
